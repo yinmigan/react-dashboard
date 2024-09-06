1926c2c732d97fcfd8bbf667d6e4e30a09a50505
Maria Michelle Eballena <maria.eballena@lanexcorp.com>
2024-09-07 00:14:46 +0800

    fixed react-dom import for version 17.0

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 
 */
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
